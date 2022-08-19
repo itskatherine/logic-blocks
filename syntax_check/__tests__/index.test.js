98c0b06d31a1c4ed0d11adb8bd85892ce679d01b
Katherine <hurstkatherine@hotmail.co.uk>
2022-08-19 09:40:11 +0100

    Passing single line chunk corruption

--- syntax_check/__tests__/index.test.js
+++ syntax_check/__tests__/index.test.js
@@ -1,8 +1,15 @@
-const {returnErrors } = require("../index")
+const { returnErrors } = require("../index");
 
 describe("return syntax error", () => {
   test("given empty string returns empty array", () => {
     expect(returnErrors("")).toEqual([]);
   });
+  test("given uncorrupted single string returns empty array", () => {
+    expect(returnErrors("<{<>}>")).toEqual([]);
+  });
+  test("given corrupted single string '<{})' returns array with ['line 1, > expected ]", () => {
+    expect(returnErrors("<{})")).toEqual(["line 1, > expected"]);
+    expect(returnErrors("<{}{}}")).toEqual(["line 1, > expected"]);
+    expect(returnErrors("<{}{}<>>(}")).toEqual(["line 1, ) expected"]);
+  });
 });
-
--- syntax_check/index.js
+++ syntax_check/index.js
@@ -1,4 +1,25 @@
 const returnErrors = (chunkStr) => {
+  const openBrackets = ["{", "[", "(", "<"];
+  const closingBrackets = ["}", "]", ")", ">"];
+  const chunkStrArr = chunkStr.split("");
+
+  let openBracketsInStr = [];
+
+  for (let i = 0; i < chunkStrArr.length; i++) {
+    let currentBracket = chunkStrArr[i];
+    if (openBrackets.includes(currentBracket)) {
+      openBracketsInStr.push(currentBracket);
+    } else {
+      const lastOpenBracket = openBracketsInStr.at(-1);
+      const lastOpenBracketIndex = openBrackets.indexOf(lastOpenBracket);
+      if (lastOpenBracketIndex === closingBrackets.indexOf(currentBracket)) {
+        openBracketsInStr.pop();
+      } else {
+        return [`line 1, ${closingBrackets[lastOpenBracketIndex]} expected`];
+      }
+    }
+  }
+
   return [];
 };
 
